b02219fe18d8d44b7a58852a499a62d3d89007c5
jexica <jexica.armas@outsourcing.softwareestrategico.com>
2020-04-22 21:17:33 -0500

    Fin de la sección 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Rutas 
+import { APP_ROUTES } from './app.routes'
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register.component';
 
 @NgModule({
   declarations: [
@@ -21,10 +26,13 @@ import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component
     Graficas1Component,
     HeaderComponent,
     SidebarComponent,
-    BreadcrumbsComponent
+    BreadcrumbsComponent,
+    PagesComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
